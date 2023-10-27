04407a69ed507f931de0a45695acbce9cc5b912f
hirokatsu-soda-lvgs <hirokatsu.soda@leverages.jp>
2023-10-27 09:53:57 +0900

    検索フォームを作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
-      <div><p>mlb-portal</p></div>
+      <div>
+        <p>mlb-portal</p>
+        <Link href='/search'>search players</Link>
+      </div>
     </main>
-  )
+  );
 }
