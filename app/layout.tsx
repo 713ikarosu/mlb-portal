7116089808593c8d3f26ee1a9d4a0d78f3de1d57
HirokatsuSouda <ikarosu0909@gmail.com>
2024-06-12 22:08:06 +0900

    impl Lp draft contents

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
+      <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </head>
       <body className={NotoSansJP.className}>
         <LayoutHeader />
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,38 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      <div className="bg-gray-200 text-slate-600 py-24">
+        <h2 className="text-3xl font-bold text-center py-8">日本のメジャーリーグファンへ</h2>
+        <div className="flex justify-center space-x-4 py-8">
+          {/* TODO: コンポーネント化 */}
+          <div className="card w-96 bg-slate-100 shadow-xl">
+            <div className="card-body text-slate-600">
+              <h2 className="card-title">MLB用語解説</h2>
+              <p>数多くあるメジャーリーグ用語を日本人向けにお届け。</p>
+            </div>
+          </div>
+
+          <div className="card w-96 bg-slate-100 shadow-xl">
+            <div className="card-body text-slate-600">
+              <h2 className="card-title">日本人MLB選手情報</h2>
+              <p>メジャーリーグで活躍する日本人選手の情報を日本人向けに紹介。</p>
+            </div>
+          </div>
+
+          <div className="card w-96 bg-slate-100 shadow-xl">
+            <div className="card-body text-slate-600">
+              <h2 className="card-title">お気に入り球団情報</h2>
+              <p>30球団あるメジャーリーグ球団からお気に入りの球団の情報をお届け。</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-24">
+        <h2 className="text-3xl font-bold py-8">新規コンテンツ鋭意制作中</h2>
+        <p>今後は、MLBの試合速報や日本人選手の試合結果などもお届け予定です。お楽しみに！</p>
+      </div>
     </main>
   );
 }
