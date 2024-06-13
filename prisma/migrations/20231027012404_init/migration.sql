5d50f952f40543eb6135cd566126f0c24e66156c
HirokatsuSouda <ikarosu0909@gmail.com>
2024-06-13 22:43:06 +0900

    Remove unnecessary files for prisma etc

--- prisma/migrations/20231027012404_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `statsId` to the `player` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "player" ADD COLUMN     "statsId" INTEGER NOT NULL;
