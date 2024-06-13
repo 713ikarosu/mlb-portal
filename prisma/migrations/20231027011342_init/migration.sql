9cc5fe63212bcdc528958b5e5875615c05f4b1d7
Hirokatsu <ikarosu0909@gmail.com>
2024-06-13 22:46:22 +0900

    Add vercel.json file (#23)
    * Add vercel.json file
    
    * Remove unnecessary files for prisma etc

--- prisma/migrations/20231027011342_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "player" (
-    "id" SERIAL NOT NULL,
-    "fullName" TEXT NOT NULL,
-    "fullNameKana" TEXT NOT NULL,
-
-    CONSTRAINT "player_pkey" PRIMARY KEY ("id")
-);
