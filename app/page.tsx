8f256bb1ebdfd2f5820a1ae0be050a201e482c8a
HirokatsuSouda <ikarosu0909@gmail.com>
2024-07-05 23:21:17 +0900

    minimal top sp style

--- app/page.tsx
+++ app/page.tsx
@@ -20,37 +20,37 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="bg-gray-200 text-slate-600 py-24">
+      <section className="bg-gray-200 text-slate-600 py-24">
         <h2 className="text-3xl font-bold text-center py-8">日本のメジャーリーグファンへ</h2>
-        <div className="flex justify-center space-x-4 py-8">
+        <div className="flex justify-center gap-4 py-8 flex-col md:flex-row">
           {/* TODO: コンポーネント化 */}
-          <div className="card w-96 bg-slate-100 shadow-xl">
+          <div className="card min-w-80 bg-slate-100 shadow-xl">
             <div className="card-body text-slate-600">
               <h2 className="card-title">MLB用語解説</h2>
               <p>数多くあるメジャーリーグ用語を日本人向けにお届け。</p>
             </div>
           </div>
 
-          <div className="card w-96 bg-slate-100 shadow-xl">
+          <div className="card min-w-80 bg-slate-100 shadow-xl">
             <div className="card-body text-slate-600">
               <h2 className="card-title">日本人MLB選手情報</h2>
               <p>メジャーリーグで活躍する日本人選手の情報を日本人向けに紹介。</p>
             </div>
           </div>
 
-          <div className="card w-96 bg-slate-100 shadow-xl">
+          <div className="card min-w-80 bg-slate-100 shadow-xl">
             <div className="card-body text-slate-600">
               <h2 className="card-title">お気に入り球団情報</h2>
               <p>30球団あるメジャーリーグ球団からお気に入りの球団の情報をお届け。</p>
             </div>
           </div>
         </div>
-      </div>
+      </section>
 
-      <div className="p-24">
+      <section className="px-4 py-16 md:p-24">
         <h2 className="text-3xl font-bold py-8">新規コンテンツ鋭意制作中</h2>
         <p>今後は、MLBの試合速報や日本人選手の試合結果などもお届け予定です。お楽しみに！</p>
-      </div>
+      </section>
     </main>
   );
 }
