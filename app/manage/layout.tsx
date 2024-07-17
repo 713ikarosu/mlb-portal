dc0ff8cbee94d7dfdfa81ce1c4f9cc3924081609
HirokatsuSouda <ikarosu0909@gmail.com>
2024-07-17 22:06:25 +0900

    Add Base page for manage page

---
+++ app/manage/layout.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+// TODO: 仮実装でwordページのものを持ってきています。
+// TODO: 管理者ログイン認証など、追加実装の予定あり。
+export default function ManageLayout({ children }: { children: ReactNode }) {
+  return <main className="flex justify-center bg-gray-200 text-slate-600">{children}</main>;
+}
---
+++ app/manage/page.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import Heading from "../_components/Heading";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false, // noindexの設定
+  },
+};
+
+export default function ManagePage() {
+  return (
+    <section className="flex space-y-8 py-16 flex-col w-full max-w-screen-md">
+      <Heading>MLBポータル - 管理画面</Heading>
+      <form className="flex space-y-4 flex-col">
+        <label className="flex flex-col">MLB用語管理画面。現在開発中。</label>
+        {/* TODO: 編集用テーブルを用意 */}
+      </form>
+    </section>
+  );
+}
