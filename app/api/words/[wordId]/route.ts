1a326eb992474a4151ef5ce5500ded0beaaa0f33
HirokatsuSouda <ikarosu0909@gmail.com>
2024-07-22 21:58:42 +0900

    Add CRUD handler

--- app/api/words/[wordId]/route.ts
+++ app/api/words/[wordId]/route.ts
@@ -14,3 +14,15 @@ export async function GET(_: NextRequest, { params }: { params: { wordId: string
   // TODO: フロントのWord型に合わせてフォーマットできると良さそう
   return Response.json({ data });
 }
+
+export async function POST(req: NextRequest, { params }: { params: { wordId: string } }) {
+  const body = await req.json();
+  const photo = await prisma.word.update({
+    where: { id: Number(params.wordId) },
+    data: {
+      word: body.word,
+      description: body.description,
+    },
+  });
+  return Response.json({ photo });
+}
