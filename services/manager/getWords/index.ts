088eaa11ad1b1524864160af5b17d20edfbb939f
HirokatsuSouda <ikarosu0909@gmail.com>
2024-08-10 12:55:00 +0900

    Add debug log

--- services/manager/getWords/index.ts
+++ services/manager/getWords/index.ts
@@ -11,6 +11,8 @@ type Props = {
 };
 
 export async function getWords({ revalidate }: Props): Promise<{ words: Word[] }> {
+  console.log("getWords url: ", path("/api/words/"));
+
   return fetch(path("/api/words/"), {
     next: {
       tags: ["words"],
