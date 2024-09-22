ddb22544b5828ab7ebb18e05fe7f88fbef9b6601
HirokatsuSouda <ikarosu0909@gmail.com>
2024-09-22 22:33:11 +0900

    update yarn, and label to heading tag

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -13,7 +13,7 @@ export default function ManagePage() {
     <section className="flex space-y-8 py-16 flex-col w-full max-w-screen-md">
       <Heading>MLBポータル - 管理画面</Heading>
       <form className="flex space-y-4 flex-col">
-        <label className="flex flex-col">MLB用語管理画面。現在開発中。</label>
+        <h2 className="flex flex-col">MLB用語管理画面。現在開発中。</h2>
         {/* TODO: 編集用テーブルを用意 */}
         <WordsTable />
       </form>
--- app/words/page.tsx
+++ app/words/page.tsx
@@ -11,7 +11,7 @@ export default function WordsPage() {
     <section className="flex space-y-8 py-16 flex-col w-full max-w-screen-md">
       <Heading>MLB用語解説（開発中）</Heading>
       <form className="flex space-y-4 flex-col">
-        <label className="flex flex-col">MLB用語を日本人ファン向けに解説します。現在調整中につき動きません。</label>
+        <h3 className="flex flex-col">MLB用語を日本人ファン向けに解説します。現在調整中につき動きません。</h3>
         {/* TODO: フォームのコンポーネント作成 */}
         <input type="text" className="bg-white rounded-md h-8" />
         <button type="submit" className="btn btn-primary">
